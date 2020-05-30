c588b9a40a6e7d44b85355f94b834a9ab1c972d7
KonradB <55328364+KonraddB@users.noreply.github.com>
2020-05-30 19:54:53 +0200

    initial commit

---
+++ js/question.js
@@ -0,0 +1,29 @@
+
+//array of objects
+const quiz = [
+    {
+        q:'xxx?',
+        options:['a','b','c','d'],
+        answer:0
+    },
+    {
+        q:'Czy kejti jest gruba?',
+        options:['tak','tak'],
+        answer:1
+    },
+    {
+        q:'aaa?',
+        options:['a','b','c'],
+        answer:2
+    },
+    {
+        q:'bbb?',
+        options:['a'],
+        answer:0
+    },
+    {
+        q:'ccc?',
+        options:['a','b','c','d','e'],
+        answer:4
+    },
+]

c0fd4ce62457dc61a8e629c1ff3995bff9883c3c
KonradB <55328364+KonraddB@users.noreply.github.com>
2020-06-03 22:31:32 +0200

    Update question.js

--- js/question.js
+++ js/question.js
@@ -4,26 +4,26 @@ const quiz = [
     {
         q:'Data rozpoczęcia pierwszej wojny światowej?',
         options:['1939','1945','1914','1918'],
-        answer:2
+        answer:3
     },
     {
         q:'W którym roku powstał standard ECMAScript 6(ES6)',
         options:['2015','2016'],
-        answer:0
+        answer:1
     },
     {
         q:'Które polecenie w git służy do zsynchrowania kopii?',
         options:['clone','fork','copy'],
-        answer:1
+        answer:2
     },
     {
         q:'Które polecenie w flexbox centruje w pionie?',
         options:['justify-content:center','align-items:center'],
-        answer:1
+        answer:2
     },
     {
         q:'Czym deklarujemy stałą?',
         options:['var','let','const'],
-        answer:2
+        answer:3
     },
 ]
